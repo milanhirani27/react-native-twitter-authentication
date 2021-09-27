bf9919162175af74483303c77affbd4659ff5887
milanhirani27 <milan.170410116023@gmail.com>
2021-09-27 13:40:44 +0530

    twitter authentication in react-native

---
+++ ios/RNTwitterSignIn.h
@@ -0,0 +1,6 @@
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <Foundation/Foundation.h>
+
+@interface RNTwitterSignIn : NSObject <RCTBridgeModule>
+@end
---
+++ ios/RNTwitterSignIn.m
@@ -0,0 +1,53 @@
+#import <TwitterKit/TWTRKit.h>
+#import <React/RCTConvert.h>
+#import <React/RCTUtils.h>
+#import "RNTwitterSignIn.h"
+
+@implementation RNTwitterSignIn
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+BOOL authNotResolved = true;
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(init: (NSString *)consumerKey consumerSecret:(NSString *)consumerSecret resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [[Twitter sharedInstance] startWithConsumerKey:consumerKey consumerSecret:consumerSecret];
+}
+RCT_EXPORT_METHOD(logIn: (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession * _Nullable session, NSError * _Nullable error) {
+        if (session) {
+            TWTRAPIClient *client = [TWTRAPIClient clientWithCurrentUser];
+
+            [client requestEmailForCurrentUser:^(NSString *email, NSError *error) {
+                NSString *requestedEmail = (email) ? email : @"";
+                NSDictionary *body = @{@"authToken": session.authToken,
+                                       @"authTokenSecret": session.authTokenSecret,
+                                       @"userID":session.userID,
+                                       @"email": requestedEmail,
+                                       @"userName":session.userName};
+                if(authNotResolved){
+                    resolve(body);
+                    authNotResolved = false;
+                }
+            }];
+        } else {
+            reject(@"Error", @"Twitter signin error", error);
+        }
+    }];
+}
+
+RCT_EXPORT_METHOD(logOut)
+{
+    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
+    NSString *userID = store.session.userID;
+    [store logOutUserID:userID];
+}
+@end
